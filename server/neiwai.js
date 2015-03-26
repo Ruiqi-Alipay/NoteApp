445d19345633cf5134ccbbac45157765e74b361d
李瑞骐 <ruiqi.li@alibaba-inc.com>
2015-03-26 22:38:53 +0800

    debug

--- server/neiwai.js
+++ server/neiwai.js
@@ -4,12 +4,16 @@ var path = require('path');
 var router = express.Router();
 
 router.get('/getlatest', function (req, res, next) {
+	console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+	console.log(req);
 	res.json({
 		data: 'Just for test, get received!'
 	});
 });
 
 router.post('/getlatest', function (req, res, next) {
+	console.log('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB');
+	console.log(req);
 	res.json({
 		data: 'Just for test, post received!'
 	});
