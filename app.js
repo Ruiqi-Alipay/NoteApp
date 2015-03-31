833f53524e394c92c521100a6074943580d525f5
李瑞骐 <ruiqi.li@alibaba-inc.com>
2015-03-31 13:11:07 +0800

    DEBUG

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'webapp', 'index.html'));
 });
 
+app.all('/*', function (req, res) {
+	console.log(req.url);
+	res.json({
+		req: req.url
+	})
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
